394ea0c95a42bb8e6cd7b471dc6519f9159f8412
Hugo <ducky4tools@gmail.com>
2025-05-13 14:15:28 +0200

    Création template login & controlleur security

---
+++ app/src/Controller/Front/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récupère la dernière erreur de login, s’il y en a
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Récupère le dernier nom d'utilisateur saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Ce code ne sera jamais exécuté
+        throw new \LogicException('Cette méthode peut rester vide - Symfony interceptera la route /logout automatiquement.');
+    }
+}
