b760d18f71843d78501b66ff02ba1b5cfb383e72
Benjamin Aubert <benjaminaubert22@gmail.com>
2025-05-16 10:16:44 +0200

    Modification des paramètres d'entrée des routes

--- app/src/Controller/Back/TweetController.php
+++ app/src/Controller/Back/TweetController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Back;
 
 use App\Dto\Request\CreateTweetRequest;
 use App\Dto\Request\UpdateTweetRequest;
+use App\Dto\Response\MessageResponse;
 use App\Entity\Tweet;
 use App\Entity\User;
+use App\Repository\TweetRepository;
+use App\Repository\UserRepository;
 use App\Repository\RetweetRepository;
 use App\Service\TweetService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +18,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use App\Dto\Response\MessageResponse;
 use OpenApi\Attributes as OA;
 
 #[OA\Tag(name: 'Tweets')]
@@ -39,11 +41,14 @@ class TweetController extends AbstractController
     #[Route('/tweets/{id}', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     #[OA\Get(summary: 'Afficher un tweet')]
-    public function show(Tweet $tweet): JsonResponse
+    public function show(int $id, TweetRepository $tweetRepo): JsonResponse
     {
-        /** @var User $currentUser */
-        $currentUser = $this->getUser();
+        $tweet = $tweetRepo->find($id);
+        if (!$tweet) {
+            return $this->json(new MessageResponse('Tweet introuvable'), 404);
+        }
 
+        $currentUser = $this->getUser();
         $response = $this->tweetService->getResponseByTweet($tweet, $currentUser);
         return $this->json($response);
     }
@@ -51,10 +56,14 @@ class TweetController extends AbstractController
     #[Route('/users/{id}/tweets', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     #[OA\Get(summary: 'Liste les tweets d’un utilisateur')]
-    public function userTweets(User $user): JsonResponse
+    public function userTweets(int $id, UserRepository $userRepo): JsonResponse
     {
-        $currentUser = $this->getUser();
+        $user = $userRepo->find($id);
+        if (!$user) {
+            return $this->json(new MessageResponse('Utilisateur introuvable'), 404);
+        }
 
+        $currentUser = $this->getUser();
         $tweets = $this->tweetService->getUserTweets($user, $currentUser);
         return $this->json($tweets);
     }
@@ -62,10 +71,14 @@ class TweetController extends AbstractController
     #[Route('/users/{id}/timeline', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     #[OA\Get(summary: 'Timeline utilisateur')]
-    public function timeline(User $user, RetweetRepository $retweetRepo): JsonResponse
+    public function timeline(int $id, UserRepository $userRepo): JsonResponse
     {
-        $currentUser = $this->getUser();
+        $user = $userRepo->find($id);
+        if (!$user) {
+            return $this->json(new MessageResponse('Utilisateur introuvable'), 404);
+        }
 
+        $currentUser = $this->getUser();
         $tweets = $this->tweetService->getUserTimeline($user, $currentUser);
         return $this->json($tweets);
     }
@@ -89,13 +102,21 @@ class TweetController extends AbstractController
     #[Route('/tweets/{id}', methods: ['PUT'])]
     #[IsGranted('ROLE_USER')]
     #[OA\Put(summary: 'Mettre à jour un tweet')]
-    public function update(Request $request, Tweet $tweet, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
-    {
-        /** @var User $currentUser */
-        $currentUser = $this->getUser();
+    public function update(
+        int $id,
+        Request $request,
+        TweetRepository $tweetRepo,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ): JsonResponse {
+        $tweet = $tweetRepo->find($id);
+        if (!$tweet) {
+            return $this->json(new MessageResponse('Tweet introuvable'), 404);
+        }
 
+        $currentUser = $this->getUser();
         if ($tweet->getAuthor() !== $currentUser) {
-            return $this->json(['error' => 'Unauthorized'], 403);
+            return $this->json(new MessageResponse('Vous n\'avez pas les droits de faire cette action'), 403);
         }
 
         $dto = $serializer->deserialize($request->getContent(), UpdateTweetRequest::class, 'json');
@@ -112,10 +133,14 @@ class TweetController extends AbstractController
     #[Route('/tweets/{id}', methods: ['DELETE'])]
     #[IsGranted('ROLE_USER')]
     #[OA\Delete(summary: 'Supprimer un tweet')]
-    public function delete(Tweet $tweet): JsonResponse
+    public function delete(int $id, TweetRepository $tweetRepo): JsonResponse
     {
-        $currentUser = $this->getUser();
+        $tweet = $tweetRepo->find($id);
+        if (!$tweet) {
+            return $this->json(new MessageResponse('Tweet introuvable'), 404);
+        }
 
+        $currentUser = $this->getUser();
         if ($tweet->getAuthor() !== $currentUser) {
             return $this->json(new MessageResponse('Vous n\'avez pas les droits de faire cette action'), 403);
         }
@@ -123,5 +148,4 @@ class TweetController extends AbstractController
         $this->tweetService->deleteTweet($tweet);
         return $this->json(new MessageResponse('Tweet supprimé avec succès !'), 200);
     }
-
 }
